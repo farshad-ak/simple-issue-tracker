15730ecef0d5572c1728507ada5a19ecfc27338e
Farshad <akbari.farshad@gmail.com>
2019-06-01 09:28:49 +0200

    Initial commit

---
+++ src/main/java/com/farshad/tracker/simpleissuetracker/SimpleIssueTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.farshad.tracker.simpleissuetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleIssueTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleIssueTrackerApplication.class, args);
+	}
+
+}
