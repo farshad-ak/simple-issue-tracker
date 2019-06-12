4b1320ecd219a34f50f30b16cd362a9ffe857636
Farshad Akbari <akbari.farshad@gmail.com>
2019-06-12 13:25:15 +0200

    add unit testing

--- src/test/java/com/farshad/tracker/simpleissuetracker/issue/assign/AssignmentControllerTest.java
+++ src/test/java/com/farshad/tracker/simpleissuetracker/issue/assign/AssignmentControllerTest.java
@@ -1,18 +1,93 @@
 package com.farshad.tracker.simpleissuetracker.issue.assign;
 
+import com.farshad.tracker.simpleissuetracker.issue.IssueTypeEnum;
+import com.farshad.tracker.simpleissuetracker.issue.story.Story;
+import com.farshad.tracker.simpleissuetracker.issue.story.StoryEstimatedPointEnum;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class AssignmentControllerTest {
 
+    @MockBean
+    private AssignmentService assignmentService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void makeAssignment() {
+    void makeAssignment() throws Exception {
+        Map<Integer, List<Story>> mockListMapStory = new HashMap<>();
+        Story story = new Story();
+        story.setId(1L);
+        story.setPoint(StoryEstimatedPointEnum.THREE.getValue());
+        story.setIssueType(IssueTypeEnum.Story.getValue());
+        story.setDeveloperId(1L);
+        story.setAssignedWeek(1);
+        List<Story> mockStoryList = new ArrayList<>();
+        mockStoryList.add(story);
+        mockListMapStory.put(1, mockStoryList);
+        doReturn(mockListMapStory).when(assignmentService).makeAssignment();
+        mockMvc.perform(get("/assignment"))
+                .andExpect(status().isOk());
+
     }
 
     @Test
-    void viewAssignment() {
+    void viewAssignment() throws Exception {
+        List<String> assignmentSummary = new ArrayList<>();
+        assignmentSummary.add("String 1");
+        assignmentSummary.add("String 2");
+        assignmentSummary.add("String 3");
+        assignmentSummary.add("String 4");
+        doReturn(assignmentSummary).when(assignmentService).getAssignmentSummary();
+        mockMvc.perform(get("/assignment/summary"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()", is(4)));
+
+//                assignmentService.getAssignmentSummary();
     }
 
     @Test
-    void getAssignmentList() {
+    void getAssignmentList() throws Exception {
+        Map<Integer, List<Story>> mockListMapStory = new HashMap<>();
+        Story story = new Story();
+        story.setId(1L);
+        story.setPoint(StoryEstimatedPointEnum.THREE.getValue());
+        story.setIssueType(IssueTypeEnum.Story.getValue());
+        story.setDeveloperId(1L);
+        story.setAssignedWeek(1);
+        List<Story> mockStoryList = new ArrayList<>();
+        mockStoryList.add(story);
+        mockListMapStory.put(1, mockStoryList);
+        doReturn(mockListMapStory).when(assignmentService).getAssignmentList();
+        mockMvc.perform(get("/assignments"))
+                .andExpect(status().isOk())
+
+                .andExpect(jsonPath("$.1.[0].id", is(1)))
+                .andExpect(jsonPath("$.1.[0].developerId", is(1)))
+                .andExpect(jsonPath("$.1.[0].point", is(3)))
+                .andExpect(jsonPath("$.1.[0].assignedWeek", is(1)));
+
     }
 }
--- src/test/java/com/farshad/tracker/simpleissuetracker/issue/assign/AssignmentServiceTest.java
+++ src/test/java/com/farshad/tracker/simpleissuetracker/issue/assign/AssignmentServiceTest.java
@@ -1,22 +1,118 @@
 package com.farshad.tracker.simpleissuetracker.issue.assign;
 
+import com.farshad.tracker.simpleissuetracker.base.dto.DeveloperPointsDto;
+import com.farshad.tracker.simpleissuetracker.developer.DeveloperService;
+import com.farshad.tracker.simpleissuetracker.issue.IssueTypeEnum;
+import com.farshad.tracker.simpleissuetracker.issue.story.Story;
+import com.farshad.tracker.simpleissuetracker.issue.story.StoryEstimatedPointEnum;
+import com.farshad.tracker.simpleissuetracker.issue.story.StoryService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 class AssignmentServiceTest {
 
+    @MockBean
+    private StoryService storyService;
+
+    @MockBean
+    private DeveloperService developerService;
+
+    @Autowired
+    private AssignmentService assignmentService;
+
+
     @Test
-    void makeAssignment() {
+    @DisplayName("Test Make Assignment")
+    void TestMakeAssignment() {
+        Story story = new Story();
+        story.setId(1L);
+        story.setPoint(StoryEstimatedPointEnum.THREE.getValue());
+        story.setIssueType(IssueTypeEnum.Story.getValue());
+
+        Map<Long, Story> mockStoryList = new HashMap<>();
+        mockStoryList.put(story.getId(), story);
+        doReturn(mockStoryList).when(storyService).getEstimatedStories();
+        doReturn(2L).when(developerService).count();
+        doReturn(story).when(storyService).getStoryWithClosestPointTo(any());
+        DeveloperPointsDto mostAvailableDeveloperandAvailablePoint = new DeveloperPointsDto(2L, 8L);
+        doReturn(mostAvailableDeveloperandAvailablePoint).when(storyService).getMostAvailableDeveloperIdForStory(any());
+        doReturn(story).when(storyService).update(any());
+
+
+        Map<Integer, List<Story>> integerListMap = assignmentService.makeAssignment();
+        assertNotNull(integerListMap);
     }
 
     @Test
-    void getAssignmentSummary() {
+    @DisplayName("Test Get Assignment Summary")
+    void testGetAssignmentSummary() {
+        doReturn(2L).when(developerService).count();
+        Story story = new Story();
+        story.setId(1L);
+        story.setPoint(StoryEstimatedPointEnum.THREE.getValue());
+        story.setIssueType(IssueTypeEnum.Story.getValue());
+
+        Map<Long, Story> mockStoryList = new HashMap<>();
+        mockStoryList.put(story.getId(), story);
+        doReturn(mockStoryList).when(storyService).getEstimatedStories();
+        doReturn(new BigInteger("10")).when(storyService).getEstimatedStoryPoints();
+
+        List<String> assignmentSummary = assignmentService.getAssignmentSummary();
+        assertNotNull(assignmentSummary);
+        assertEquals(4, assignmentSummary.size());
     }
 
     @Test
-    void getRemaningPointsInWeek() {
+    @DisplayName("Test Get Remaning Points In Week")
+    void testGetRemainingPointsInWeek() {
+        Integer mockRemainingPointsInWeek = 20;
+        doReturn(5L).when(storyService).getEstimatedPointCountForWeek(1);
+        Integer remainingPointsInWeek = assignmentService.getRemainingPointsInWeek(WeeksEnum.WEEK_ONE, 2);
+        assertEquals(15, remainingPointsInWeek.intValue());
+
     }
 
     @Test
-    void getAssignmentList() {
+    @DisplayName("Test Get Assignment List")
+    void testGetAssignmentList() {
+        Map<Integer, List<Story>> mockAssignListMap = new HashMap<>();
+        Story story1 = new Story();
+        story1.setId(1L);
+        story1.setPoint(StoryEstimatedPointEnum.THREE.getValue());
+        story1.setIssueType(IssueTypeEnum.Story.getValue());
+        story1.setDeveloperId(1L);
+        story1.setAssignedWeek(1);
+
+        Story story2 = new Story();
+        story2.setId(2L);
+        story2.setPoint(StoryEstimatedPointEnum.FIVE.getValue());
+        story2.setIssueType(IssueTypeEnum.Story.getValue());
+        story2.setDeveloperId(2L);
+        story2.setAssignedWeek(1);
+        List<Story> mockStories = new ArrayList<>();
+        mockAssignListMap.put(1, mockStories);
+        doReturn(mockAssignListMap).when(storyService).getAssignmentList();
+
+        Map<Integer, List<Story>> assignmentList = assignmentService.getAssignmentList();
+        assertNotNull(assignmentList);
+        assertEquals(1, assignmentList.size());
     }
 }
